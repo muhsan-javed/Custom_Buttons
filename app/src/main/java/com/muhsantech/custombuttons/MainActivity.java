27b57ff70c2b4f8176d3189e25a359d051d86242
MuhsanJaved <muhsantech048@outlook.com>
2022-02-01 16:14:54 +0500

    Floating Text Button

--- app/src/main/java/com/muhsantech/custombuttons/MainActivity.java
+++ app/src/main/java/com/muhsantech/custombuttons/MainActivity.java
@@ -3,12 +3,21 @@ package com.muhsantech.custombuttons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import ru.dimorinny.floatingtextbutton.FloatingTextButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingTextButton floatingTextButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        floatingTextButton = findViewById(R.id.action_button);
+        
+        floatingTextButton.setOnClickListener(view -> {
+            Toast.makeText(this, "Button is Clicked", Toast.LENGTH_SHORT).show();
+        });
     }
 }
